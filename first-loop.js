b5597128bc87254902df715e163b57f9cca873e3
Rakib55555 <rakibulislamrakibprince@gmail.com>
2021-08-09 19:50:24 +0600

    secound commit

---
+++ first-loop.js
@@ -0,0 +1,14 @@
+// loop//
+
+//while loop
+// var i = 0;
+// while(i<=500){
+//     console.log(i);
+//     console.log('ajka loop kormu reh');
+//     i++;
+// }
+
+//for loop
+for(var i = 0; i<=300; i++){
+    console.log(i)
+}
