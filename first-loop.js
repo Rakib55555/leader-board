f15d9bc9d633c5e3338d332d5ee0ecba971c8d20
Rakib55555 <rakibulislamrakibprince@gmail.com>
2021-08-09 19:51:26 +0600

    secound-remasterd commit

--- first-loop.js
+++ first-loop.js
@@ -1,12 +1,12 @@
-// loop//
+//////////////// loop//////////////////
 
 //while loop
-// var i = 0;
-// while(i<=500){
-//     console.log(i);
-//     console.log('ajka loop kormu reh');
-//     i++;
-// }
+var i = 0;
+while(i<=500){
+    console.log(i);
+    console.log('ajka loop kormu reh');
+    i++;
+}
 
 //for loop
 for(var i = 0; i<=300; i++){
